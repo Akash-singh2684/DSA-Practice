1ada7788fa02f11430c75d39788f1ada184739cf
Akash Singh <141215054+Akash-singh2684@users.noreply.github.com>
2025-08-15 18:04:22 +0530

    Time: 3 ms (76.74%) | Memory: 50.3 MB (67.53%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-       Integer majority1 = 0;
+        Integer majority1 = 0;
         Integer majority2 = 0;
-        int count1 = 0;
-        int count2 = 0;
-
-        for (int num : nums) {
+        int count1=0;
+        int count2=0;
+        int n = nums.length;
+        for(int num : nums){
             if (num == majority1) {
                 count1++;
             } else if (num == majority2) {
@@ -21,28 +21,22 @@ class Solution {
                 count2--;
             }
         }
-
         count1 = 0;
         count2 = 0;
-
-        for (int num : nums) {
-            if (num == majority1) {
+        for( int num : nums){
+            if(num == majority1){
                 count1++;
-            } else if (num == majority2) {
+            }else if(num == majority2){
                 count2++;
             }
         }
-
         List<Integer> res = new ArrayList<>();
-        int n = nums.length;
-
-        if (count1 > n / 3) {
+        if(count1 > n/3 ){
             res.add(majority1);
         }
-        if (count2 > n / 3) {
+         if(count2 > n/3){
             res.add(majority2);
         }
-
-        return res;        
+        return res;
     }
 }
